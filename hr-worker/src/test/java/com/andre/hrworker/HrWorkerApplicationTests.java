b453316c0ffaa737fd1df04a7e2b148483e2b5e0
André Luis <andrelluis17@hotmail.com>
2022-03-23 16:54:33 -0300

    Projeto Hr-worker create

---
+++ hr-worker/src/test/java/com/andre/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
